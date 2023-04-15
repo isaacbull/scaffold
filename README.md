f81d48de21c356871d2151f3a91195fe1c413fd7
Isaac bull <isaacbull122@outlook.com>
2023-04-15 15:41:50 +0000

    Adding initial structure

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a readme file for creating a python scaffol
---
+++ hello.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+
+# var=
+# x=1
+# y=2
+# result = add(x, y)
+# print(f"This is the sum: {x}, {y}, {result}")
+result = add(1, 2)
+print("This is the sum: 1, 2, %s" % result)
